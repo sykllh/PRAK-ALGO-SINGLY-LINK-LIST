73139ba2af35ad041476779c8f9111a89ba9d74e
sykllh <sykllhidmi@gmail.com>
2026-04-24 15:25:54 +0700

    biar di ulang

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -26,5 +26,14 @@ public:
 
         Node *nodeBaru = new Node;
         nodeBaru->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if ((START != NULL) && (nim == START->noMhs))
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return
+            }
+        }
     }
 }
